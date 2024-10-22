fa1dd5230879830d2c47ff375f8bc5b42366939a
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-10-22 14:02:58 +0400

    Added highest temp

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -343,7 +343,7 @@ function weather(changelang) {
 		//pour la description et temperature
 		$(".w_desc_meteo").text(data.weather[0].description + ". ");
 		$(".w_desc_temp").text(Math.floor(data.main.temp) + '° ');
-	
+		$(".w_desc_temp_max").text(Math.floor(data.main.temp_max) + '° ')
 
 		//pour l'icone
 		var d_n = dayOrNight(data.sys.sunset, data.sys.sunrise);
